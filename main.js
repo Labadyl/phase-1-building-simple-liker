85dcb515c463aad03eac542fa6375f9a9d4a4617
Lesley Labady <labadylesley@gmail.com>
2022-08-09 23:20:31 -0400

    Done

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+likes()
 
+document.addEventListener("DOMContentLoaded", () => {
+})
 
+const errorModal = document.querySelector("#modal")
+errorModal.classList.add('hidden')
 
+function likes(){
+  const likeB = document.querySelectorAll(".like-glyph")
+
+  likeB.forEach((l) => {
+    l.addEventListener("click",(event)=> console.log("hey"))
+  })
+  mimicServerCall()
+  .then(resp => '' )
+  .catch(error => {
+    errorModal.classList.remove('hidden')
+    setTimeout(()=>{
+      errorModal.classList.add('hidden')
+    },3000)
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
